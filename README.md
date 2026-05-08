2c1de927dec628c040d11d4b3ed42664c3e86ad2
Harshalatha-K <harshalatha.22cse@cambridge.edu.in>
2026-05-08 22:16:48 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+
+
+# Face Mask Detection using CNN
+
+This project detects whether a person is wearing a face mask or not using a lightweight CNN model and Streamlit.
+
+## Features
+- Lightweight model
+- TensorFlow Lite
+- Streamlit Cloud deployment
+- Image upload prediction
+
---
+++ app.py
@@ -0,0 +1,56 @@
+
+
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+import cv2
+from PIL import Image
+
+# Load labels
+with open("labels.txt", "r") as f:
+    labels = [line.strip() for line in f.readlines()]
+
+# Load TFLite model
+interpreter = tf.lite.Interpreter(model_path="model.tflite")
+
+interpreter.allocate_tensors()
+
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+IMG_SIZE = 64
+
+st.title("Face Mask Detection App")
+
+uploaded_file = st.file_uploader(
+    "Upload an Image",
+    type=["jpg", "jpeg", "png"]
+)
+
+if uploaded_file is not None:
+
+    image = Image.open(uploaded_file)
+
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+    img = np.array(image)
+
+    img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
+
+    img = img / 255.0
+
+    img = np.expand_dims(img.astype(np.float32), axis=0)
+
+    interpreter.set_tensor(input_details[0]['index'], img)
+
+    interpreter.invoke()
+
+    prediction = interpreter.get_tensor(output_details[0]['index'])
+
+    if prediction[0][0] > 0.5:
+        result = labels[1]
+    else:
+        result = labels[0]
+
+    st.success(f"Prediction: {result}")
+
